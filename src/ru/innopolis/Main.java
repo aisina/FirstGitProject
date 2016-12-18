ddd3c25e0bdc28267065b05e6194bf207c569806
aisina <aisinakukmor@mail.ru>
2016-12-18 10:42:27 +0300

    Initial commit

---
+++ src/ru/innopolis/Main.java
@@ -0,0 +1,11 @@
+package ru.innopolis;
+
+/**
+ * Created by innopolis on 18.12.2016.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+    }
+}
